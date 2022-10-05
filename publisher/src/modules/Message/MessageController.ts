7c38a5215ab3db5ef44b1af6c58578db12a7136b
Henrique Tardivo <henrique.tardivo@gmail.com>
2022-10-04 21:52:41 -0300

    bugfix

--- publisher/src/modules/Message/MessageController.ts
+++ publisher/src/modules/Message/MessageController.ts
@@ -1,18 +1,17 @@
-import { Request, Response} from "express"
-import { AppError } from "../../shared/error/AppError"
-import { publish } from "queue/publish"
+import { Request, Response } from "express";
+import { AppError } from "../../shared/error/AppError";
+import { publish } from "../../queue/publish";
 
-export class MessageController{
-    async handle(request: Request, response: Response): Promise<Response>{
-        try {
-            const { mensagem } = request.body
+export class MessageController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { mensagem } = request.body;
 
-            const publicou = await publish(mensagem)
+      const publicou = await publish(mensagem);
 
-            return response.status(200).json({publicou: publicou})
-    
-        } catch (err){
-            throw new AppError("Erro ao postar mensagem ", 401)
-        }
+      return response.status(200).json({ publicou: publicou });
+    } catch (err) {
+      throw new AppError("Erro ao postar mensagem ", 401);
     }
+  }
 }
--- publisher/src/queue/publish.ts
+++ publisher/src/queue/publish.ts
@@ -1,18 +1,19 @@
-import config from "./config.json"
-import { createBrokerAsPromised} from "rascal"
+import config from "./config.json";
+import { createBrokerAsPromised } from "rascal";
+import { AppError } from "../shared/error/AppError";
 
-export async function publish(message: string): Promise<boolean>{
-    try {
-        const broker = await  createBrokerAsPromised(config)
+export async function publish(message: string): Promise<boolean> {
+  try {
+    const broker = await createBrokerAsPromised(config);
 
-        await broker.publish("message", message)
+    await broker.publish("message", message);
 
-        console.log("mensagem publicada!")
+    console.log("mensagem publicada!");
 
-        return true
-    } catch (error) {
-        console.log(error)
+    return true;
+  } catch (error) {
+    console.log(error);
 
-        return false
-    }
+    return false;
+  }
 }
